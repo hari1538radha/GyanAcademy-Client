59e6dce1b39aa7775b0c462216fc7f6929009ad0
DHARUNDP5 <dharunprakashkdp@gmail.com>
2022-11-05 16:03:03 +0530

    courseLibrary page responsive for cards & image finished [Dharun]

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'md': {'min': '768px', 'max': '1023px'},
+
+      'sm':  {'min': '360px', 'max': '423px'},
+    },
     extend: {},
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
