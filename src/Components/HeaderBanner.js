c405200e33928c2f17bfbab043613187af14d76b
DHARUNDP5 <dharunprakashkdp@gmail.com>
2022-11-10 12:40:50 +0530

    courseLibrary page responsive 480-768 done [Dharun]

--- src/Components/HeaderBanner.js
+++ src/Components/HeaderBanner.js
@@ -8,10 +8,11 @@ const HeaderBanner = (props) => {
         className="bg-no-repeat w-screen h-64 bg-cover flex items-center text-center justify-center
          md:h-[223px] md:bg-cover md:bg-center
          sm:h-[112px] sm:bg-cover sm:bg-center
-         xl:h-[223px] xl:bg-cover xl:bg-center"
+         xl:h-[223px] xl:bg-cover xl:bg-center
+         sl:h-[112px] sl:bg-cover sl:bg-center"
         style={{ backgroundImage: `url(${props.bgImg})` }}
       >
-        <h3 className="text-6xl text-white text-[40px] font-bold sm:text-[17px]">
+        <h3 className="text-6xl text-white text-[40px] font-bold sm:text-[17px] sl:text-[30px]">
           {props.BannerText}
         </h3>
       </div>
