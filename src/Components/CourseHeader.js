dac3c02e22160cef1c7f268b49ff8a7800c37dac
DHARUNDP5 <dharunprakashkdp@gmail.com>
2022-11-10 09:33:30 +0530

    courseLibrary page responsive all done [Dharun]

--- src/Components/CourseHeader.js
+++ src/Components/CourseHeader.js
@@ -5,12 +5,12 @@ function CourseHeader() {
     <div>
       {" "}
       <div className="flex font-medium text-[#18191F] flex-row mt-[24px] w-[1162px] border-b-[1px] border-gray-400 h-[40px]
-         md:border-b-[1px] md:w-[764px] 
-         xl:w-[955px]
-         sm:w-[100%] sm:text-[12px] sm:mt-[10px] sm:h-[35px] sm:mr-[38px]">
+         md:border-b-[1px] md:w-[764px] md:font-semibold
+         xl:w-[955px] xl:font-semibold
+         sm:w-[100%] sm:text-[12px] sm:mt-[10px] sm:h-[35px] sm:mr-[38px] sm:font-semibold">
         {["Most Popular"].map((obj) => (
           <div className="flex sm:gap-[25px]">
-            <span className="ml-12 h-full border-b-[3px] border-[#9D1C7F] p-[5px] xl:ml-8 sm:ml-0">
+            <span className="ml-12 h-full border-b-[3px] border-[#9D1C7F] p-[5px] text-[#9D1C7F] xl:ml-8 sm:ml-0">
               <h3>{obj}</h3>
             </span>
             <span className="ml-12 h-full p-[5px] sm:ml-0 ">
