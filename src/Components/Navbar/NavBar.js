0e5010aa7bbf7c6c7ffb62cc8b362204a0c4d2bb
Senthamizharasu-MS <senthamizharasum.ms2020cse@sece.ac.in>
2022-11-11 12:19:22 +0530

    Responsive for all resolution completerd (Header,Footer,Home page,Application Form) [Arasu]

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -11,30 +11,31 @@ function Header() {
   return (
     <div>
 
-      <div className="justify-between items-center w-screen h-[72px] shadow-header_box flex pl-[147px] md:pl-[36px] sm:h-[36px] sm:pl-[17px] ">
-        <div className="flex items-center justify-center sm:pt-[4px]">
-          <img src={Logo} className="pr-3 sm:pr-[5px] sm:h-[20px]" />
+      <div className="justify-between items-center w-screen h-[72px] shadow-header_box flex pl-[147px] md:pl-[36px] sl:h-[36px] sl:pl-[17px] sm:h-[36px] sm:pl-[17px] xl:pl-[90px] ">
+        <div className="flex items-center justify-center sl:pt-[4px] sm:pt-[4px]">
+          <img alt="" src={Logo} className="pr-3 sl:pr-[5px] sl:h-[20px] sm:pr-[5px] sm:h-[20px]" />
           <div
 
             onClick={handelHomePage}
-            className="flex gap-2 cursor-pointer font-bold text-[24.69px] md:text-[20px] sm:text-[12px] sm:gap-1 "
+            className="flex gap-2 cursor-pointer font-bold text-[24.69px] md:text-[20px] sl:text-[17px] sl:gap-1 sm:text-[12px] sm:gap-1 "
           >
             <p className=" text-[#9D1C7F]">Gyan</p>
             <p className=" text-[#2B2A85]">Academy</p>
           </div>
         </div>
-        <div className="flex sm:gap-[10px]">
-          <div className="pt-[23px] pb-[22px] pr-[30px] text-[16.46px] text-[#2B2A85] md:pr-[30px] md:text-[14px] sm:text-[10px] sm:pr-[0px]">
-            <ul className="flex gap-[30px] md:gap-[27px] sm:gap-[20px] ">
-              <a className="sm:h-[13px]" href="/courselibrary">BootCamps</a>
+        <div className="flex sl:gap-[10px] items-center sm:gap-[10px]">
+          <div className="pt-[23px] pb-[22px] pr-[30px] text-[16.46px] text-[#2B2A85] md:pr-[30px] md:text-[14px] sl:text-[12px] sl:pt-0 sl:pb-0 sl:pr-[0px]  sm:text-[10px] sm:pr-[0px]">
+            <ul className="flex gap-[30px] md:gap-[27px] sl:gap-[10px] sm:gap-[20px] ">
+              <a className="sl:h-[13px] sm:h-[13px]" href="/courselibrary">BootCamps</a>
+              <a className="sl:h-[13px] sm:h-[13px]" href="/gallery">Gallery</a>
               {/* <a className="sm:h-[12px] sm:w-[93px]" href="">Learning Path</a>
               <a className="sm:h-[12px] sm:w-[51px]" href="">Pricing</a>
               <a className="sm:h-[12px] sm:w-[43px]" href="">About</a> */}
             </ul>
           </div>
-          <div className="mt-[22px] mr-[130px] sm:mr-[16px] sm:mt-[14px]">
+          <div className="mr-[130px] md:mr-[70px] sl:mr-[16px] sm:mr-[16px] xl:mr-[100px]">
             <a href="https://gyanmatrix.com/contact-us/" className="text-[16px] font-bold bg-gradient-to-b from-[#282B85] to-[#9A1D80] text-white px-[22px] py-[10px] rounded-[6px]
-            sm:text-[9px] sm:px-[10px] sm:py-[5px] sm:rounded-[3px]">
+            sl:text-[9px] sl:px-[10px] sl:py-[5px] sl:rounded-[3px] sm:text-[9px] sm:px-[10px] sm:py-[5px] sm:rounded-[3px]">
             Contact Us</a>
           </div>
         </div>
